8c2cfff8367a5e98bdffc2478c41eaf6d5cc19fd
Shunya Nagashima <syun864297531@gmail.com>
2022-11-28 17:26:05 +0900

    list added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import List from './list';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+            <Route path="/" element={<List />} />
+            {/* <Route path="company" element={<Company />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
