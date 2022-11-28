76189a148f14ac4fef7baed6877b5fb361b85ea0
nsns245 <suginao1234@gmail.com>
2022-11-28 21:09:36 +0900

    aaaa

--- src/Display.js
+++ src/Display.js
@@ -8,7 +8,7 @@ class Display extends React.Component {
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.text=this.props.text
         this.props.wordList.map(word=>{
             this.text=this.text.replace(new RegExp(word,'g'),`<span className="ImportantWord">${word}</span>`);
@@ -19,7 +19,7 @@ class Display extends React.Component {
         return(
             <div className="DisplayContent">
                 <h2>{this.props.date}</h2> 
-                <p>文章量:{this.props.text.length}/目安時間:{int(this.props.text.length/400)}分</p>
+                <p>文章量:{this.props.text.length}/目安時間:{Math.floor(this.props.text.length/400)}分</p>
                 <div dangerouslySetInnerHTML={{ __html:  this.text}} />
             </div>
         )
