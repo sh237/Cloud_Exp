0c766ca599802a9ca6225c7b3c0dfb0df0aea46d
nsns245 <suginao1234@gmail.com>
2022-12-05 12:17:33 +0900

    要約

--- src/Display.js
+++ src/Display.js
@@ -7,7 +7,8 @@ class Display extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            text:""
+            text:"",
+            summary:""
         }
     }
 
@@ -19,6 +20,45 @@ class Display extends React.Component {
             text=text.replace(new RegExp(word,'g'),`<span className="ImportantWord">${word}</span>`);
         });
         this.setState({text});
+
+        const texts_=this.props.data.text.split('。');
+        console.log(texts_);
+        const imList=[]
+        texts_.map(text_=>{
+            let c=0;
+            this.props.data.wordList.map(word=>{
+                let l=text_.match(new RegExp(word,'g'));
+                if(l){
+                    c+=l.length;
+                }
+            })
+            imList.push(c);
+        });
+        console.log(imList);
+        const m = (imList.length / 2) | 0;
+
+        const imList2 = imList.concat();;
+        imList2.sort();
+        console.log("sort",imList2);
+        let median=0;
+        if (imList2.length % 2) { 
+            median=imList2[m]
+        }else{
+            median=(imList2[m - 1] + imList2[m])/2;
+        }
+        console.log(median);
+
+        console.log(imList2,imList);
+
+        let summary="";
+        texts_.map((content,i)=>{
+            if (imList[i]>=median){
+                console.log("#",summary);
+                summary=summary+content;
+            }
+        });
+        this.setState({summary});
+        
     }
     
     render() {
@@ -33,15 +73,15 @@ class Display extends React.Component {
                     <thead>
                         <tr>
                             <th>日付</th>
-                            <th>元の文章</th>
                             <th>重要な文字を強調した文章</th>
+                            <th>要約</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>{this.props.data.date}</td>
-                            <td>{this.props.data.text}</td>
                             <td><div dangerouslySetInnerHTML={{ __html:  this.state.text}} /></td>
+                            <td>{this.state.summary}</td>
                         </tr>
                     </tbody>
                 </table>
